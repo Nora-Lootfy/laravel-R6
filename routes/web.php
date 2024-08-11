804240b06e5182b91847736b2188510f85f0bc6f
Nourhan Abdelaziz <nourhan33abdelaziz@gmail.com>
2024-08-11 10:32:35 +0300

    changing routes

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ExampleController;
-use App\Http\Controllers\CarController;
+
+Route::group(['namespace' => 'App\\Http\\Controllers'], function () {
+    //  CarController routes
+    Route::resource('cars', 'CarController');
+    Route::group([
+        'prefix' => 'cars',
+        'as' => 'cars.',
+        'controller' => 'CarController',
+    ], function () {
+        Route::get('trashed/get', 'showDeleted')->name('showDeleted');
+        Route::patch('{car}', 'restore')->name('restore');
+        Route::put('{car}', 'update')->name('update');
+        Route::delete('{car}/delete', 'forceDelete')->name('forceDelete');
+    });
+
+    // ExampleController routes
+    Route::group([
+        'controller' => 'ExampleController',
+    ], function () {
+        Route::get('uploadForm', 'uploadForm');
+        Route::post('upload', 'upload')->name('upload');
+        Route::get('index', 'index');
+        Route::get('about', 'about');
+    });
+});
 
 
-Route::prefix('cars')->group(function() {
-    Route::get('', [CarController::class, 'index'])->name('cars.index');
-    Route::get('create', [CarController::class, 'create'])->name('cars.create');
-    Route::post('', [CarController::class, 'store'])->name('cars.store');
-    Route::get('{car}/edit', [CarController::class, 'edit'])->name('cars.edit');
-    Route::put('{car}', [CarController::class, 'update'])->name('cars.update');
-    Route::delete('{id}/delete', [CarController::class, 'destroy'])->name('cars.destroy');
-    Route::get('trashed', [CarController::class, 'showDeleted'])->name('cars.showDeleted');
-    Route::patch('{id}', [CarController::class, 'restore'])->name('cars.restore');
-    Route::delete('{id}', [CarController::class, 'forceDelete'])->name('cars.forceDelete');
-    
-}); 
-
-
-Route::get('uploadForm', [ExampleController::class, 'uploadForm']);
-Route::post('upload', [ExampleController::class, 'upload'])->name('upload');
-
-Route::get('index', [ExampleController::class, 'index']);
-Route::get('about', [ExampleController::class, 'about']);
